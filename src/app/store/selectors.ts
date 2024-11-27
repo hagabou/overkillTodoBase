7c30eafb3ba63a7a46472fe68873631510019308
Hagabou <haga2@hotmail.fr>
2024-11-27 07:23:59 +0100

    fix todo initial order

--- src/app/store/selectors.ts
+++ src/app/store/selectors.ts
@@ -5,7 +5,10 @@ export const getState = createFeatureSelector<State>(featureKey);
 
 export const selectTodos = createSelector(
   getState,
-  (state: State) => state.todos,
+  (state: State) => [
+    ...state.todos.filter(todo => !todo.isClosed),
+    ...state.todos.filter(todo => todo.isClosed),
+  ]
 );
 
 export const selectTodoByTitle = (title: string) =>
